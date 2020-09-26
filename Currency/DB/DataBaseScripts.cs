e86a0bee249168ce8afab07b52328f6542673218
Mask <rebootroot13@gmail.com>
2020-09-26 15:37:37 +0300

    Finished

---
+++ Currency/DB/DataBaseScripts.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Linq;
+using System.Diagnostics;
+using System.Data.SQLite;
+using System.Data.Common;
+
+
+namespace Currency
+{
+    class DataBase
+    {
+        private string dbFileName;
+        private SQLiteConnection connection;
+        private SQLiteCommand command;
+        private string tableName;
+
+        
+
+
+        public DataBase()
+        {
+            this.dbFileName = "data.sqlite";
+            this.connection = new SQLiteConnection($"Data Source={dbFileName};Version=3;");
+            this.command = new SQLiteCommand();
+            this.tableName = "Currency";
+            CreateDataBase();
+        }
+
+        public string Select(string date, string charCode)
+        {
+            string ret = null;
+            try
+            {
+                connection.Open();
+                command.Connection = connection;
+                //I thought that Select request would be enough
+                string sqlQuery = $"SELECT DISTINCT value FROM {tableName} WHERE (date = '{date}' AND charcode = '{charCode}')";
+                command = new SQLiteCommand(sqlQuery, connection);
+                var data = command.ExecuteReader();
+                while (data.Read())
+                {
+                    ret += data.GetString(0);
+                }
+                connection.Close();
+                return ret;
+
+            }
+            catch (SQLiteException ex)
+            {
+                return "Error: " + ex.Message;
+            }
+        }
+
+        public void CreateDataBase()
+        {
+
+            if (!File.Exists(dbFileName))
+            {
+                SQLiteConnection.CreateFile(dbFileName);
+            }
+
+            try
+            {
+                connection.Open();
+                command.Connection = connection;
+                
+                command.CommandText = $"CREATE TABLE IF NOT EXISTS {tableName} (date TEXT, value TEXT, charcode TEXT, CONSTRAINT Cur UNIQUE  (date,value,charcode))";
+                SQLiteFunction.RegisterFunction(typeof(string));
+
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine($"Table {tableName} didn't create");
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        public void Insert(string value, string charCode, string date)
+        {
+            try
+            {
+                connection.Open();
+                command.Connection = connection;
+
+                command.CommandText = $"INSERT OR IGNORE INTO {tableName} (date, charcode, value) VALUES ('{date}', '{charCode}','{value}')";
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine("Data didn't insert");
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+
+        public void DropTable()
+        {
+            connection.Open();
+            command.Connection = connection;
+            command.CommandText = $"DROP TABLE {tableName}";
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+    }
+}
---
+++ Currency/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Linq;
+using System.Diagnostics;
+using System.Data.SQLite;
+using System.Data.Common;
+
+
+
+namespace Currency
+{
+    class Program
+    {
+        async static private void Start()
+        {
+            string valute = "GBP";
+
+            string today = GetDate();
+            DataBase dataBase = new DataBase();
+            Parse parse = new Parse(GetData(GetDate()));
+            Console.WriteLine(dataBase.Select(GetDate(), valute));
+            while (today == GetDate())
+            {
+                if (!(today == GetDate()))
+                {
+                    today = GetDate();
+                    parse = new Parse(GetData(GetDate()));
+                    Console.WriteLine(dataBase.Select(GetDate(), valute));
+                    
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Start();
+        }
+        private static string GetData(string date)
+        {
+            string url = $"http://www.cbr.ru/scripts/XML_daily.asp?date_req={date}";
+            using (var webclient = new WebClient())
+            {
+                var response = webclient.DownloadString(url);
+                return response;
+            }
+        }
+        private static string GetDate()
+        {
+            DateTime _date = DateTime.Today;
+            StringBuilder date = new StringBuilder();
+
+            if (_date.Day.ToString().Length == 1)
+                date.Append("0");
+            date.Append(_date.Day.ToString());
+            date.Append(".");
+            if (_date.Month.ToString().Length == 1)
+                date.Append("0");
+            date.Append(_date.Month.ToString());
+            date.Append(".");
+            date.Append(_date.Year.ToString());
+
+            return date.ToString();
+        }
+    }
+
+    class Parse
+    {
+        string xml;
+
+        public Parse(string xml)
+        {
+            this.xml = xml;
+            Handler();
+        }
+
+        private void Handler()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            XmlElement date = doc.DocumentElement;
+            DataBase dataBase = new DataBase();
+            foreach (XmlNode valute in date)
+            {
+                dataBase.Insert(
+                    valute.SelectSingleNode("./Value").InnerText,
+                    valute.SelectSingleNode("./CharCode").InnerText,
+                    date.Attributes["Date"].Value
+                    );
+            }
+        }
+    }
+}
